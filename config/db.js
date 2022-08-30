5d42fbe312399a90cbd889542769ece0c380c55d
Meateater02 <dqh.phan@gmail.com>
2022-08-31 00:11:27 +1200

    Created react app

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const username = process.env.NAME;
+const password = process.env.PASSWORD;
+
+const db = "mongodb+srv://" + username + ":" + password + "@cluster4.puqtqqc.mongodb.net/?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      db,
+      {
+        useNewUrlParser: true
+      }
+    );
+
+    console.log('MongoDB is Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
