daa61d38263b28b3a869de55cd1d1d5262764c00
Hung Duy Le <93351359+ThePieLegend@users.noreply.github.com>
2022-09-30 00:22:46 +1300

    WIP: Display data in a table
    updating page structure to dispaly data pulled from database.

--- frontend/src/pages/Moderator.js
+++ frontend/src/pages/Moderator.js
@@ -1,12 +1,72 @@
-import React from "react";
+import React, { Component } from 'react';
+import '../App.css';
+import axios from 'axios';
+// import { Link } from 'react-router-dom';
+// import BookCard from './BookCard';
 
-const Moderator = () =>  {
+class Moderator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('/api/articles')
+      .then(res => {
+        this.setState({
+          articles: res.data
+        })
+      })
+      .catch(err =>{
+        console.log('Error from Admin class');
+      })
+  };
+
+
+  render() {
+    const articles = this.state.articles;
+    console.log("PrintArticle: " + articles);
+    let articleList;
+
+    // if(!articles) {
+    //   articleList = "there is no article record!";
+    // } else {
+    //   articleList = articles.map((article, k) =>
+    //   //need to change this line below:
+    //     <BookCard book={article} key={k} />
+    //   );
+    // }
+
     return (
-     <div>
-       <h2>Moderate an Article</h2>
-           <p>Moderators will be using this page to check the articles that has been submitted</p>
-       </div>
-   );
+      <div className="ShowArticleList">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <br />
+              <h2 className="display-4 text-center">Articles List</h2>
+            </div>
+
+            <div className="col-md-11">
+              {/* <Link to="/create-book" className="btn btn-outline-warning float-right">
+                + Add New Article
+              </Link> */}
+              <br />
+              <br />
+              <hr />
+            </div>
+
+          </div>
+
+          <div className="list">
+                {articleList}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Moderator;
