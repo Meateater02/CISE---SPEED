cab9a2b45493223446eb70775a56fd3cc0888217
Meateater02 <dqh.phan@gmail.com>
2022-10-05 00:17:53 +1300

    Added unit test for submit button

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+// import App from "./App";
+import userEvent from "@testing-library/user-event";
+const { default: SubmitForm } = require("./pages/SubmitForm");
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("fill in the input box to check whether it functions well", () => {
+  render(<SubmitForm />);
+  userEvent.type(screen.getByPlaceholderText(/title/i), "Article Testing Name");
+});
+
+test("submit button is enabled to click", () => {
+  render(<SubmitForm />);
+  expect(screen.getByRole("button", { name: /submit/i })).toBeEnabled();
 });
