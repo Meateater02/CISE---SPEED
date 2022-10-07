f30a1d8f6f48d33d87157476b6430e787368e490
Meateater02 <dqh.phan@gmail.com>
2022-10-07 22:23:48 +1300

    WIP: testing table component

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,5 +1,9 @@
 import { render, screen } from "@testing-library/react";
 // import App from "./App";
+import Table from "./components/table";
+import tablecolumns from "./components/admincolumn";
+import { testData } from "./mockData/testData";
+
 import userEvent from "@testing-library/user-event";
 const { default: SubmitForm } = require("./pages/SubmitForm");
 
@@ -17,4 +21,11 @@ test("fill in the input box to check whether it functions well", () => {
 test("submit button is enabled to click", () => {
   render(<SubmitForm />);
   expect(screen.getByRole("button", { name: /submit/i })).toBeEnabled();
+});
+
+test("testing table", () => {
+  render(<Table columns={tablecolumns} data={testData} />);
+  expect(screen.getByRole("table")).toHaveLength(1);
+  expect(screen.getByRole("tr")).toHaveLength(testData.length);
+  expect(screen.getByRole("th")).toHaveLength(tablecolumns.length);
 });
