28c098f422fbaebdd0ce8bb05acc777be6a1bd3c
Hung Duy Le <93351359+ThePieLegend@users.noreply.github.com>
2022-10-14 22:20:02 +1300

    Done: unit test for app.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
@@ -11,18 +12,46 @@ describe("Base template is rendering without errors", () => {
     expect(headingElement).toBeInTheDocument();
   });
 
-  test("name of the software is displayed", async () => {
-    render(<App />);
-    const headingElement = screen.getAllByRole("heading");
-    // screen.debug(headingElement);
-    expect(headingElement).toBeTruthy();
-  });
-
   test("navbar is present", () => {
     render(<App />);
     const navbarElement = screen.getByRole("list");
     // screen.debug(navbarElement);
     expect(navbarElement).toBeInTheDocument();
+  });
+
+  test("there are five options in the navbar", () => {
+    render(<App />);
+    const navbarElement = screen.getAllByRole("listitem");
+    expect(navbarElement).toHaveLength(5);
+  });
+
+  test("navlinks are working", () => {
+    render(<App />);
+    // first option
+    expect(screen.getByText("Search Articles").closest("a")).toHaveAttribute(
+      "href",
+      "/"
+    );
+    // second option
+    expect(screen.getByText("Submit an Article").closest("a")).toHaveAttribute(
+      "href",
+      "/SubmitArticle"
+    );
+    // third option
+    expect(screen.getByText("Moderator").closest("a")).toHaveAttribute(
+      "href",
+      "/Moderator"
+    );
+    // fourth option
+    expect(screen.getByText("Analyst").closest("a")).toHaveAttribute(
+      "href",
+      "/Analyst"
+    );
+    // fifth option
+    expect(screen.getByText("Admin").closest("a")).toHaveAttribute(
+      "href",
+      "/Admin"
+    );
   });
 });
 
