3f6ca3c45b5013666d42c1c5494ccd6610a3cd46
Meateater02 <dqh.phan@gmail.com>
2022-10-14 23:17:37 +1300

    Done: unit test for drop down option

--- frontend/src/pages/__test__/SearchArticle.test.js
+++ frontend/src/pages/__test__/SearchArticle.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import SearchArticle from "./MockReactSelect";
 
 describe("Test react-select component", () => {
@@ -10,10 +10,44 @@ describe("Test react-select component", () => {
     // screen.debug(screen.getByTestId("selectOptionTest"));
   });
 
-  test("should call onChange function when user select an option", async () => {
-    render(<SearchArticle />);
+  test("there is a placeholder in the dropdown list", async () => {
+    const { getByText } = render(<SearchArticle />);
+    const placeholder = getByText("Select...");
+    expect(placeholder).toBeTruthy();
+  });
+
+  test("options listed are correct", async () => {
+    const { getByText } = render(<SearchArticle />);
     const selectElement = screen.getByTestId("selectOptionTest");
+
     expect(selectElement).toBeDefined();
-    expect(selectElement).not.toBeNull();
+
+    //first option = TDD
+    fireEvent.keyDown(selectElement.firstChild, { key: "ArrowDown" });
+    await waitFor(() => {
+      expect(getByText("TDD")).toBeInTheDocument();
+    });
+    fireEvent.click(getByText("TDD"));
+
+    //second option = Mob Programming
+    fireEvent.keyDown(selectElement.firstChild, { key: "ArrowDown" });
+    await waitFor(() => {
+      expect(getByText("Mob Programming")).toBeInTheDocument();
+    });
+    fireEvent.click(getByText("Mob Programming"));
+
+    //third option = TFD
+    fireEvent.keyDown(selectElement.firstChild, { key: "ArrowDown" });
+    await waitFor(() => {
+      expect(getByText("TFD")).toBeInTheDocument();
+    });
+    fireEvent.click(getByText("TFD"));
+
+    //fourth option = BDD
+    fireEvent.keyDown(selectElement.firstChild, { key: "ArrowDown" });
+    await waitFor(() => {
+      expect(getByText("BDD")).toBeInTheDocument();
+    });
+    fireEvent.click(getByText("BDD"));
   });
 });
